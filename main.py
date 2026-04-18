052325fcd3157271fd3e2854f018ed37c3d63564
zillydillyMe <zanithuni@gmail.com>
2026-04-18 19:53:49 +0200

    feat: create game loop

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill('black')
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()

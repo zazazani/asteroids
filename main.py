769dd92397c565ba6f63013ed9b413b612f75001
zillydillyMe <zanithuni@gmail.com>
2026-04-20 21:47:27 +0200

    move asteroids

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x =  SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,7 +26,14 @@ def main():
         
         screen.fill('black')
 
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
+
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
+        # print(f"dt {dt}")
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,31 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, 'white', self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
